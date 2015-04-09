c9759eb339ad249fc8e4d2d7e390c1459c58459a
Jasterrr <ivan.bernatovic.93@gmail.com>
2015-04-09 19:27:29 +0200

    Added links to single article
    Anchor tag is added in h1 tag, article title is link

--- resources/views/articles/articles.blade.php
+++ resources/views/articles/articles.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 @foreach($articles as $article)
-	<h1>{{ $article->title }}</h1>
+	<h1><a href="{{ route('showArticle', $article->id) }}">{{ $article->title }}</a></h1>
 	<p>{{ $article->body }}</p>
 @endforeach
 @stop
