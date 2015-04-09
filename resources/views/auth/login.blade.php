1953ae17ee7acd787bd7f09768e926631fae88a9
Jasterrr <ivan.bernatovic.93@gmail.com>
2015-04-09 13:20:43 +0200

    Added all inputs to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,8 +2,20 @@
 
 @section('content')
 
-{!! Form::open() !!}
-
-{!! Form::close() !!}
+<div class="col-sm-12 col-md-5 col-lg-5">
+	{!! Form::open(['url' => route('postLogin')]) !!}
+		<div class="form-group">
+			{!! Form::label('username', 'Username:') !!}
+			{!! Form::text('username', null, ['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('password', 'Password:') !!}
+			{!! Form::password('password',  ['class' => 'form-control']) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::submit('Log in',  ['class' => 'btn btn-primary form-control']) !!}
+		</div>
+	{!! Form::close() !!}
+</div>
 
 @stop
