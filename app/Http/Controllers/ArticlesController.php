ebe1189f894212b474ebbff7cc37de1c0d223e32
Jasterrr <ivan.bernatovic.93@gmail.com>
2015-04-09 19:12:14 +0200

    Article view and article controller are updated to display all articles
    All articles are now sent from ArticleController@showArticles to
    article/article view and ordered by latest.

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,12 +4,16 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
-
+use App\Article;
 class ArticlesController extends Controller {
 
 	public function showArticles(){
+		$articles = Article::latest()->get();
+
 		return view('articles/articles')
-			->with(['title' => 'Home']);
+			->with(['title' => 'Home',
+					'articles' => $articles
+					]);
 	}
 
 }
--- resources/views/articles/articles.blade.php
+++ resources/views/articles/articles.blade.php
@@ -1,14 +1,8 @@
 @extends('master')
 
 @section('content')
-
-<div>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-</div>
-
+@foreach($articles as $article)
+	<h1>{{ $article->title }}</h1>
+	<p>{{ $article->body }}</p>
+@endforeach
 @stop
