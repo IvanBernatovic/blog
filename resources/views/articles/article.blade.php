943a25b135249d8f62a3e006d940572863d9d35a
Jasterrr <ivan.bernatovic.93@gmail.com>
2015-04-10 15:28:05 +0200

    In user.blade.php view authors name is now link to profile

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <h1>{{ $article->title }}</h1>
-<h3>Author: {{ $article->user->username }}</h3>
+<h3>Author: <a href="{{ route('user', $article->user->username) }}">{{ $article->user->username }}</a></h3>
 <p>{{ $article->body }}</p>
 
 @stop
