e60bb5472cebd9d59e1041a2ee83412b35d770b4
Jasterrr <ivan.bernatovic.93@gmail.com>
2015-04-11 12:24:41 +0200

    Username

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 
 @section('content')
+<p class="header">{!! $user->username !!}'s articles:</p>
 @foreach($user->articles as $article)
 	<h1><a href="{{ route('showArticle', $article->id) }}">{{ $article->title }}</a></h1>
 	<p>{{ $article->body }}</p>
